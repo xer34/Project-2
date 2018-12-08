5b1474109bda7d5e0277e2cf56378a17f6bc51cf
Xer34 <experimental@gmail.com>
2018-12-08 08:48:07 -0500

    routes still broken

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+const passport = require('passport');
 
 module.exports = function(app) {
   // Get all examples
@@ -34,4 +35,12 @@ module.exports = function(app) {
       res.json(dbExample);
     });
   });
+
+  app.post("/api/login", passport.authenticate("google"), function(req, res) {
+    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
+    // So we're sending the user back the route to the members page because the redirect will happen on the front end
+    // They won't get this or even be able to access this page if they aren't authed
+    res.json("/home");
+  });
 };
+
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,33 @@
-var db = require("../models");
+var isAuthenticated = require("../config/isAuthenticated");
+const passport = require("passport");
+
 module.exports = function(app) {
   app.get("/", function(req, res) {
     res.render("index");
   });
-  app.get("/home", function(req, res) {
-    res.render("home");
-  });
+
   app.get("/classes", function(req, res) {
     res.render("classes");
   });
-  app.get("*", function(req, res) {
-    res.render("404");
+ 
+  app.get("/home", isAuthenticated, function(req, res) {
+    res.render("home");
   });
+
+  // app.get(
+  //   "/auth/google",
+  //   passport.authenticate("google", { scope: ["profile", "email"] })
+  // );
+
+  // app.get(
+  //   "/auth/google/redirect",
+  //   passport.authenticate("google", {
+  //     successRedirect: "/home",
+  //     failureRedirect: "/404"
+  //   })
+  // );
+
+  // app.get("*", function(req, res) {
+  //   res.render("404");
+  // });
 };
