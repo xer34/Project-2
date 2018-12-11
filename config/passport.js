371c60b0979c720079de46b5af735dae4e9cabd6
Xer34 <experimental@gmail.com>
2018-12-11 16:34:39 -0500

    heroku

--- config/passport.js
+++ config/passport.js
@@ -20,33 +20,33 @@ passport.use(
       callbackURL: "https://quiet-castle-43092.herokuapp.com/auth/google/redirect"
     },
     function(token, tokenSecret, profile, done) {
-      //   db.Player.findOrCreate({
-      //     where: {
-      //       googleId: profile.id,
-      //       name: profile.displayName,
-      //       xp: 0,
-      //       level: 0
-      //     }
-      db.Player.findOne({ googleId: profile.id }).then(currentUser => {
-        if (currentUser) {
-          // already have this user
-        //   console.log("current user is: ", currentUser);
-          done(null, currentUser);
-          // do something
-        } else {
-          // if not, create user in our db
-          db.Player.create({
+        db.Player.findOrCreate({
+          where: {
             googleId: profile.id,
             name: profile.displayName,
             xp: 0,
             level: 0
-          })
-            .save()
-            .then(newUser => {
-            //   console.log("new user created: " + newUser);
-              done(null, newUser);
-            });
-        }
+          }
+      // db.Player.findOne({ googleId: profile.id }).then(currentUser => {
+      //   if (currentUser) {
+      //     // already have this user
+      //   //   console.log("current user is: ", currentUser);
+      //     done(null, currentUser);
+      //     // do something
+      //   } else {
+      //     // if not, create user in our db
+      //     db.Player.create({
+      //       googleId: profile.id,
+      //       name: profile.displayName,
+      //       xp: 0,
+      //       level: 0
+      //     })
+      //       .save()
+      //       .then(newUser => {
+      //       //   console.log("new user created: " + newUser);
+      //         done(null, newUser);
+      //       });
+      //   }
       });
     }
   )
