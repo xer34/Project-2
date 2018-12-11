5a7955457e9ca0df3a3814afbe3e1a412b1e04d9
Xer34 <experimental@gmail.com>
2018-12-11 17:39:47 -0500

    heroku

--- config/passport.js
+++ config/passport.js
@@ -22,38 +22,34 @@ passport.use(
       callbackURL: "/auth/google/redirect"
     },
     function(token, tokenSecret, profile, done) {
-        db.Player.findOrCreate({
-          where: {
+        // db.Player.findOrCreate({
+        //   where: {
+        //     googleId: profile.id,
+        //     name: profile.displayName,
+        //     xp: 0,
+        //     level: 0
+        //   }
+      db.Player.findOne({ googleId: profile.id }).then(currentUser => {
+        if (currentUser) {
+          // already have this user
+        //   console.log("current user is: ", currentUser);
+          done(null, currentUser);
+          // do something
+        } else {
+          // if not, create user in our db
+          db.Player.create({
             googleId: profile.id,
             name: profile.displayName,
             xp: 0,
             level: 0
-          }
-      // db.Player.findOne({ googleId: profile.id }).then(currentUser => {
-      //   if (currentUser) {
-      //     // already have this user
-      //   //   console.log("current user is: ", currentUser);
-      //     done(null, currentUser);
-      //     // do something
-      //   } else {
-      //     // if not, create user in our db
-      //     db.Player.create({
-      //       googleId: profile.id,
-      //       name: profile.displayName,
-      //       xp: 0,
-      //       level: 0
-      //     })
-      //       .save()
-      //       .then(newUser => {
-      //       //   console.log("new user created: " + newUser);
-      //         done(null, newUser);
-      //       });
-      //   }
-      })
-                  .then(Player => {
-              console.log("new user created: " + newUser);
-              done(null, Player);
+          })
+            .save()
+            .then(newUser => {
+            //   console.log("new user created: " + newUser);
+              done(null, newUser);
             });
+        }
+      })
     }
   )
 );
